7146789a0cc08b556ad5a0473c2798566412bdd8
João Victor <jv782063@gmail.com>
2020-08-30 17:25:53 -0300

    cria classe abstrata das operações globais crud

---
+++ src/Models/Crud.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'Connection.php';
+
+abstract class Crud extends Connection
+{
+  protected $table;
+  protected $cdName;
+  protected $connection;
+
+  abstract public function insert();
+  abstract public function update();
+
+  public function __construct()
+  {
+    $this->connection = new Connection();
+  }
+
+  public function find($id)
+  {
+    $sql = "SELECT * FROM $this->table WHERE $this->cdName = :id";
+    $stmt = $this->connection->getInstance()->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    return $stmt->fetch();
+  }
+
+  public function findAll()
+  {
+    $sql = "SELECT * FROM $this->table";
+    $stmt = $this->connection->getInstance()->prepare($sql);
+    $stmt->execute();
+
+    return $stmt->fetchAll();
+  }
+
+  public function delete($id)
+  {
+    $sql = "DELETE FROM $this->table WHERE $this->cdName = :id";
+    $stmt = $this->connection->getInstance()->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+  }
+}
