4a811d5a184e3f2c5942b705e74b0f3cac94226b
João Victor <jv782063@gmail.com>
2020-09-16 09:55:53 -0300

    cria a classe para ser usada nas pesquisas (barra de busca)

---
+++ src/Models/Pesquisa.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'Connection.php';
+
+class Pesquisa extends Connection
+{
+  protected $connection;
+
+  public function __construct()
+  {
+    $this->connection = new Connection();
+  }
+
+  public function findAll($itemASerPesquisado)
+  {
+    echo $itemASerPesquisado;
+    $sql = 'SELECT DISTINCT(l.cd_livro), l.nm_livro, l.ano_lancamento
+      FROM tb_livro l, tb_editora e, tb_autor a
+      WHERE nm_livro like CONCAT("%", :item, "%")
+      OR 
+          a.nm_autor like CONCAT("%", :item, "%")
+      AND  l.id_autor = a.cd_autor
+      OR
+      e.nm_editora like CONCAT("%", :item, "%")
+      AND  l.id_editora = e.cd_editora';
+    $smtp = $this->connection->getInstance()->prepare($sql);
+    $smtp->bindParam(':item', $itemASerPesquisado);
+    $smtp->execute();
+
+    return $smtp->fetchAll();
+  }
+}
